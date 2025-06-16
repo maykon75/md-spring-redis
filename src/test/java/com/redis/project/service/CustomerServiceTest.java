a29d61bac4c99b155740c8edb587e97b53131ffb
maykon75 <113359191+maykon75@users.noreply.github.com>
2025-06-16 11:10:28 -0300

    Update CustomerServiceTest.java

--- src/test/java/com/redis/project/service/CustomerServiceTest.java
+++ src/test/java/com/redis/project/service/CustomerServiceTest.java
@@ -1,6 +1,5 @@
 package com.redis.project.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
